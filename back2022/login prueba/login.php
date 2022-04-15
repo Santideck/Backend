29a310d62cdb6c065e7a1d25e2f10fbcc9c39ea8
Santideck <90145367+Santideck@users.noreply.github.com>
2022-04-14 22:25:26 -0300

    Add files via upload

---
+++ back2022/login prueba/login.php
@@ -0,0 +1,16 @@
+<?php
+$user = $_POST["user"];
+
+$pass = $_POST["pass"];
+
+$usuario = "admin";
+$contrasenia = "1234";
+
+if( $usuario == $user && $contrasenia == $pass ) {
+header("location:https://www.google.com/");
+} else {
+  header("location:error.html");
+}
+
+
+ ?>
